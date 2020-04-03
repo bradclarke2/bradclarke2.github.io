7da4edcbe5a575f62e0d1a356d1ac41957a1c092
Clarke, Bradley J <Bradley.J.Clarke@uk.tesco.com>
2020-04-03 18:40:16 +0100

    remove d3 and get simple websit working

--- main.js
+++ main.js
@@ -8,99 +8,11 @@ function toggleNav() {
     burger.classList.toggle('fa-times');
     nav.classList.toggle('nav-active');
 }
-
 // Calling the function after click event occurs
 burger.addEventListener('click', function () {
     toggleNav();
 });
 
-function gearAngleChange() {
-    const mainBody = document.getElementById('main-body');
-    let s = mainBody.scrollTop;
-    let d = mainBody.offsetHeight;
-    let scrollPercent = (s / (d)) * 100;
-    let theta = scrollPercent * 360;
-
-    var angle = theta, transform = function (d) {
-        return "rotate(" + angle / d.radius + ")";
-    };
-    d3.selectAll("path").attr("transform", transform);
-    console.log(theta);
-};
-
 document.addEventListener("DOMContentLoaded", function (event) {
-
-    const width = document.getElementById('gear-block').offsetWidth,
-        height = document.getElementById('gear-block').offsetHeight,
-        radius = 80,
-        x = Math.sin(2 * Math.PI / 3),
-        y = Math.cos(2 * Math.PI / 3);
-
-    var svg = d3.select("#gear-block").append("svg")
-        .attr("width", width)
-        .attr("height", height)
-        .append("g")
-        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(.55)")
-        .append("g");
-
-    var frame = svg.append("g")
-        .datum({radius: Infinity});
-
-    frame.append("g")
-        .attr("class", "annulus")
-        .datum({teeth: 80, radius: -radius * 5, annulus: true})
-        .append("path")
-        .attr("d", gear);
-
-    frame.append("g")
-        .attr("class", "sun")
-        .datum({teeth: 16, radius: radius})
-        .append("path")
-        .attr("d", gear);
-
-    frame.append("g")
-        .attr("class", "planet")
-        .attr("transform", "translate(0,-" + radius * 3 + ")")
-        .datum({teeth: 32, radius: -radius * 2})
-        .append("path")
-        .attr("d", gear);
-
-    frame.append("g")
-        .attr("class", "planet")
-        .attr("transform", "translate(" + -radius * 3 * x + "," + -radius * 3 * y + ")")
-        .datum({teeth: 32, radius: -radius * 2})
-        .append("path")
-        .attr("d", gear);
-
-    frame.append("g")
-        .attr("class", "planet")
-        .attr("transform", "translate(" + radius * 3 * x + "," + -radius * 3 * y + ")")
-        .datum({teeth: 32, radius: -radius * 2})
-        .append("path")
-        .attr("d", gear);
-
-    function gear(d) {
-        var n = d.teeth,
-            r2 = Math.abs(d.radius),
-            r0 = r2 - 8,
-            r1 = r2 + 8,
-            r3 = d.annulus ? (r3 = r0, r0 = r1, r1 = r3, r2 + 20) : 20,
-            da = Math.PI / n,
-            a0 = -Math.PI / 2 + (d.annulus ? Math.PI / n : 0),
-            i = -1,
-            path = ["M", r0 * Math.cos(a0), ",", r0 * Math.sin(a0)];
-        while (++i < n) path.push(
-            "A", r0, ",", r0, " 0 0,1 ", r0 * Math.cos(a0 += da), ",", r0 * Math.sin(a0),
-            "L", r2 * Math.cos(a0), ",", r2 * Math.sin(a0),
-            "L", r1 * Math.cos(a0 += da / 3), ",", r1 * Math.sin(a0),
-            "A", r1, ",", r1, " 0 0,1 ", r1 * Math.cos(a0 += da / 3), ",", r1 * Math.sin(a0),
-            "L", r2 * Math.cos(a0 += da / 3), ",", r2 * Math.sin(a0),
-            "L", r0 * Math.cos(a0), ",", r0 * Math.sin(a0));
-        path.push("M0,", -r3, "A", r3, ",", r3, " 0 0,0 0,", r3, "A", r3, ",", r3, " 0 0,0 0,", -r3, "Z");
-        return path.join("");
-    }
+    scrollPosition = document.getElementById('main-body').scrollTop;
 });
-
-
-
-
