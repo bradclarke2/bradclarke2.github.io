34600425249469dff1d82e2b066aa89aec584054
Clarke, Bradley J <Bradley.J.Clarke@uk.tesco.com>
2020-05-24 13:30:10 +0100

    changed to typescript

--- main.ts
+++ main.ts
@@ -2,17 +2,15 @@
 const burger = document.querySelector('.burger i');
 const nav = document.querySelector('.nav');
 
-// Defining a function
-function toggleNav() {
+function toggleNav(): void {
     burger.classList.toggle('fa-bars');
     burger.classList.toggle('fa-times');
     nav.classList.toggle('nav-active');
 }
-// Calling the function after click event occurs
 burger.addEventListener('click', function () {
     toggleNav();
 });
 
-document.addEventListener("DOMContentLoaded", function (event) {
-    scrollPosition = document.getElementById('main-body').scrollTop;
+document.addEventListener("DOMContentLoaded", function (event: Event) {
+    document.getElementById('main-body').scrollTop;
 });
